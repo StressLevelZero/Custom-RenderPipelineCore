31fd396721dba61b69bd2ca77c0acf2adb96316e
Error-mdl <20629398+Error-mdl@users.noreply.github.com>
2025-09-17 16:18:07 -0700

    Fixed path to version number incrementer

--- CommitAndArchive.sh
+++ CommitAndArchive.sh
@@ -1,7 +1,7 @@
 #printf "$@"
 git show -s --format=%H > prevGitCommitHash.txt
 git add ./prevGitCommitHash.txt
-./../IncrementVersionNumber.exe package.json
+./../../Tools/IncrementVersionNumber/IncrementVersionNumber.exe package.json
 git add ./package.json
 git commit -m "$@"
 git archive --format=tar.gz -o ./../../RenderingPackageArchives/com.unity.render-pipelines.core.tar.gz HEAD
