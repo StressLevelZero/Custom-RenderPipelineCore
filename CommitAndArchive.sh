840cbb87cf4d343c6d01a0921eed1da786cc4c60
Error-mdl <20629398+Error-mdl@users.noreply.github.com>
2025-09-17 16:13:51 -0700

    Updated commit bash script to not actually push commit, moved location of output tarball and version number incrementer

--- CommitAndArchive.sh
+++ CommitAndArchive.sh
@@ -4,5 +4,4 @@ git add ./prevGitCommitHash.txt
 ./../IncrementVersionNumber.exe package.json
 git add ./package.json
 git commit -m "$@"
-git archive --format=tar.gz -o ./../com.unity.render-pipelines.core.tar.gz HEAD
-git push
+git archive --format=tar.gz -o ./../../RenderingPackageArchives/com.unity.render-pipelines.core.tar.gz HEAD
